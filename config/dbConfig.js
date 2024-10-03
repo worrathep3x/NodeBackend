ab5d4c2d3db9aa740089dca0b1af7f9d45b716f4
worrathep3x <youhirock3x@gmail.com>
2024-10-03 19:41:42 +0700

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "dpg-crc7g908fa8c73erb91g-a.singapore-postgres.render.com",
-  USER: "admin",
-  PASSWORD: "yjbqJsyLnuCSKvADLkU2EOoO8UsInswc",
-  DB: "ams_database_5rq7",
+  HOST: "",
+  USER: "",
+  PASSWORD: "",
+  DB: "",
   dialect: "postgres",
   PORT:"5432",
   pool: {
